6b6750b6765d00b22769047f253913d478dc0a1e
NicolasKwi <nicolas.kwi@gmail.com>
2022-09-19 13:53:03 +0200

    m-j slider 1

--- public/js/index.js
+++ public/js/index.js
@@ -4,7 +4,7 @@ const sliders_cible = document.getElementById("slides_conteneur");
 
 const Scroll_sui = () => {
   let scrollX1 = sliders_cible.scrollLeft + sliders_cible.offsetWidth;
-  if (sliders_cible.scrollLeftMax < scrollX1) {
+  if (sliders_cible.scrollWidth - sliders_cible.clientWidth < scrollX1) {
     sliders_cible.scrollLeft = 0;
   } else {
     sliders_cible.scrollLeft = scrollX1;
@@ -14,23 +14,24 @@ const Scroll_sui = () => {
 const Scroll_pre = () => {
   let scrollX2 = sliders_cible.scrollLeft - sliders_cible.offsetWidth;
   if (scrollX2 < 0) {
-    sliders_cible.scrollLeft = sliders_cible.scrollLeftMax;
+    sliders_cible.scrollLeft =
+      sliders_cible.scrollWidth - sliders_cible.clientWidth;
   } else {
     sliders_cible.scrollLeft = scrollX2;
   }
 };
 
-/*let interval_Slide = setInterval(Scroll_sui, 7000);*/
+let interval_Slide = setInterval(Scroll_sui, 7000);
 
 btn_sui_Scroll.onclick = function () {
   Scroll_sui();
-  /*clearInterval(interval_Slide);
-  interval_Slide = setInterval(Scroll_sui, 7000);*/
+  clearInterval(interval_Slide);
+  interval_Slide = setInterval(Scroll_sui, 7000);
 };
 btn_pre_Scroll.onclick = function () {
   Scroll_pre();
-/*  clearInterval(interval_Slide);
-  interval_Slide = setInterval(Scroll_sui, 7000);*/
+    clearInterval(interval_Slide);
+  interval_Slide = setInterval(Scroll_sui, 7000);
 };
 /********************************/
 
